2f5ab19dc224d2a41f695c84776a49bade54827c
Gil <myrraws@yahoo.com.br>
2023-10-16 00:15:01 -0300

    Create contatos.c

---
+++ primeiro_programa/contatos.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int i = 0;
+    while(i < 10)
+    {
+        printf("%i \n", i);
+        i++;
+    }
+
+    for(int j = 0 ; j < 10; j++)
+    {
+        printf("%i \n", j);
+    }
+
+
+}
